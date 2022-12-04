e7141c2c8717cfafa1960a399a1080f366bb7b1c
Prabir Singh <prabirsinghdigital@gmail.com>
2022-12-04 17:29:12 +0530

    Learning api

---
+++ app.js
@@ -0,0 +1,6 @@
+fetch("https://www.boredapi.com/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("activity-name").textContent = data.activity
+    })
