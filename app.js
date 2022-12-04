713cd29c37e551619ef2d254acc423eeb0dbe634
Prabir Singh <prabirsinghdigital@gmail.com>
2022-12-04 17:57:42 +0530

    project beutified

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-fetch("https://www.boredapi.com/api/activity")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById("activity-name").textContent = data.activity
+function getMessage() {
+  fetch('https://www.boredapi.com/api/activity')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      document.getElementById('activity-name').textContent = data.activity
     })
+}
